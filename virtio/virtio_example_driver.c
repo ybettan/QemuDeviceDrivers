f46313911498b065429d6d678a9be604bf96a21a
Yoni Bettan <ybettan@redhat.com>
2018-12-25 15:30:35 +0200

    virtio_example_driver.c : Repaired a bug on remove.
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>

--- virtio/virtio_example_driver.c
+++ virtio/virtio_example_driver.c
@@ -6,6 +6,7 @@
 #include <asm/io.h>             /* io map */
 #include <linux/dma-mapping.h>  /* DMA */
 #include <linux/kernel.h>       /* kstrtoint() func */
+#include <linux/virtio_config.h>       /* find_single_vq() func */
 
 
 #define VIRTIO_ID_EXAMPLE 21
@@ -18,7 +19,7 @@
 //static void __iomem *io, *mem, *irq, *dma_base;
 
 /* kobject for sysfs use */
-static struct kobject *example_kobj;
+//static struct kobject *example_kobj;
 
 ///* implementation is at the buttom */
 //static struct pci_driver example;
@@ -26,21 +27,35 @@ static struct kobject *example_kobj;
 /* for IRQ support - handler write the result here when IRQ fired */
 //uint64_t io_data, mem_data;
 //uint64_t virtio_data;
-uint8_t data_to_device, data_from_device;
+//uint8_t data_to_device, data_from_device;
 
 ///* for DMA support */
 //void *dma_buf_virtual_addr;
 //dma_addr_t dma_buf_physical_addr;
 //
+//FIXME: change to non-global
+struct virtqueue *vq;
 
 //-----------------------------------------------------------------------------
 //                  sysfs - give user access to driver
 //-----------------------------------------------------------------------------
 
-static ssize_t example_show(struct kobject *kobj, struct kobj_attribute *attr,
-                              char *buf);
-static ssize_t example_store(struct kobject *kobj, struct kobj_attribute *attr,
-                              const char *buf, size_t count);
+//static ssize_t example_show(struct kobject *kobj, struct kobj_attribute *attr,
+//                              char *buf);
+//static ssize_t example_store(struct kobject *kobj, struct kobj_attribute *attr,
+//                              const char *buf, size_t size);
+static ssize_t
+virtio_buf_store(struct device *dev, struct device_attribute *attr,
+        const char *buf, size_t count)
+{
+    return count;
+}
+
+static ssize_t
+virtio_buf_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+    return 1;
+}
 
 /*
  * define a new attribute (a "file" in sysfs) type when _ATTR is:
@@ -53,9 +68,10 @@ static ssize_t example_store(struct kobject *kobj, struct kobj_attribute *attr,
  *     .store= _store, \
  * }
  */
-struct kobj_attribute example_attr =
-    /* The user can't receive WRITE access in this MACRO */
-    __ATTR(virtio_buf, 0664, example_show, example_store);
+//struct kobj_attribute example_attr =
+//    /* The user can't receive WRITE access in this MACRO */
+//    __ATTR(virtio_buf, 0664, example_show, example_store);
+static DEVICE_ATTR_RW(virtio_buf);
 
 
 /*
@@ -63,105 +79,127 @@ struct kobj_attribute example_attr =
  * as follows:
  */
 struct attribute *example_attrs[] = {
-    &example_attr.attr,
+    //&example_attr.attr,
+    &dev_attr_virtio_buf.attr,
     //&example_attr_io.attr,
     //&example_attr_mem.attr,
     NULL,
 };
-
-
-/* The above attributes are then given to the attribute group as follows: */
-struct attribute_group example_attr_group = {
+//
+//
+/////* The above attributes are then given to the attribute group as follows: */
+////struct attribute_group example_attr_group = {
+////    .attrs = example_attrs,
+////};
+//
+static const struct attribute_group example_attr_group = {
+    // directory's name
+    .name = "example",
     .attrs = example_attrs,
 };
 
 
 
-static ssize_t example_show(struct kobject *kobj, struct kobj_attribute *attr,
-                              char *buf)
-{
-    //if (attr == &example_attr_io) {
-    //    return sprintf(buf, "%llu\n", io_data);
-    //}
-
-    //if (attr == &example_attr_mem) {
-    //    return sprintf(buf, "%llu\n", mem_data);
-    //}
-
-    //return -EPERM;
-    //
-
-    return sprintf(buf, "%u\n", data_from_device);
-}
-
-static ssize_t example_store(struct kobject *kobj, struct kobj_attribute *attr,
-                              const char *buf, size_t count)
-{
-    int num;
-
-    /* convert buffer to byte */
-    if (kstrtoint(buf, 10, &num)) {
-        pr_alert("faild to convert the input into a byte on write\n");
-    }
-
-    /* only number <= 254 are supported (the result should be 1 byte max) */
-    if (num > BYTE_MAX_SIZE-1) {
-        pr_alert("supports only numbers in range [0:254] - 1 byte size");
-    }
-
-    data_to_device = num;
-
-//
-//    if (attr == &example_attr_io) {
-//
-//        /* invalidate the old data and write the new one*/
-//        io_data = 0;
-//        iowrite8(num, io);
-//
-//        /* ehco 3 > filename is trying to write 2 chars {'3', ' '} */
-//        return count;
-//    }
-//
-//    if (attr == &example_attr_mem) {
-//
-//        /* invalidate the old data and write the new one*/
-//        mem_data = 0;
-//        iowrite8(num, mem);
-//
-//        /* ehco 3 > filename is trying to write 2 chars {'3', ' '} */
-//        return count;
-//    }
-//
-//    return -EPERM;
-        return count;
-}
-
-
-
-////-----------------------------------------------------------------------------
-////                              IRQ functions
-////-----------------------------------------------------------------------------
-//
-//static irqreturn_t example_irq_handler(int irq_num, void *dev_id)
+//static ssize_t example_show(struct kobject *kobj, struct kobj_attribute *attr,
+//                              char *buf)
 //{
-//    if (ioread8(irq)) {
+//    //if (attr == &example_attr_io) {
+//    //    return sprintf(buf, "%llu\n", io_data);
+//    //}
 //
-//        /*
-//         * copy the data from relevant pipes to local variable - will wait
-//         * there until the user will read the values from there
-//         */
-//        io_data = ioread8(io);
-//        mem_data = *(uint64_t*)dma_buf_virtual_addr;
+//    //if (attr == &example_attr_mem) {
+//    //    return sprintf(buf, "%llu\n", mem_data);
+//    //}
 //
-//        /* deassert IRQ */
-//        iowrite8(0, irq);
-//        return IRQ_HANDLED;
+//    //return -EPERM;
+//    //
 //
-//    } else {
+//    //return sprintf(buf, "%u\n", data_from_device);
+//    return 1;
+//}
+
+//static ssize_t example_store(struct kobject *kobj, struct kobj_attribute *attr,
+//                              const char *buf, size_t size)
+//{
+//    int num;
+//	struct scatterlist sg;
 //
-//        /* if the IRQ port is 0 than another device caused the IRQ */
-//        return IRQ_NONE;
+//    /* convert buffer to byte */
+//    if (kstrtoint(buf, 10, &num)) {
+//        pr_alert("faild to convert the input into a byte on write\n");
 //    }
+//
+//    /* only number <= 254 are supported (the result should be 1 byte max) */
+//    if (num > BYTE_MAX_SIZE-1) {
+//        pr_alert("supports only numbers in range [0:254] - 1 byte size");
+//    }
+//
+//    //data_to_device = num;
+//
+//    pr_alert("num to store is %d\n", num);
+//    //{
+//    //    char *buf2 = kmalloc(5, GFP_KERNEL);
+//	//    sg_init_one(&sg, buf2, size);
+//    //    kfree(buf2);
+//    //}
+//
+//	///* There should always be room for one buffer. */
+//	//virtqueue_add_inbuf(vq, &sg, 1, buf, GFP_KERNEL);
+//
+//    ///* notify the device */
+//	//virtqueue_kick(vq);
+//
+////
+////    if (attr == &example_attr_io) {
+////
+////        /* invalidate the old data and write the new one*/
+////        io_data = 0;
+////        iowrite8(num, io);
+////
+////        /* ehco 3 > filename is trying to write 2 chars {'3', ' '} */
+////        return size;
+////    }
+////
+////    if (attr == &example_attr_mem) {
+////
+////        /* invalidate the old data and write the new one*/
+////        mem_data = 0;
+////        iowrite8(num, mem);
+////
+////        /* ehco 3 > filename is trying to write 2 chars {'3', ' '} */
+////        return size;
+////    }
+////
+////    return -EPERM;
+//        return size;
+//}
+
+
+
+//-----------------------------------------------------------------------------
+//                              IRQ functions
+//-----------------------------------------------------------------------------
+
+//static void example_irq_handler(struct virtqueue *vq)
+//{
+//    //if (ioread8(irq)) {
+//
+//    //    /*
+//    //     * copy the data from relevant pipes to local variable - will wait
+//    //     * there until the user will read the values from there
+//    //     */
+//    //    io_data = ioread8(io);
+//    //    mem_data = *(uint64_t*)dma_buf_virtual_addr;
+//
+//    //    /* deassert IRQ */
+//    //    iowrite8(0, irq);
+//    //    return IRQ_HANDLED;
+//
+//    //} else {
+//
+//    //    /* if the IRQ port is 0 than another device caused the IRQ */
+//    //    return IRQ_NONE;
+//    //}
 //}
 
 
@@ -169,13 +207,14 @@ static ssize_t example_store(struct kobject *kobj, struct kobj_attribute *attr,
 //                             driver functions
 //-----------------------------------------------------------------------------
 
+
 //static int example_probe(struct pci_dev *dev, const struct pci_device_id *id)
 static int example_probe(struct virtio_device *dev)
 {
     //u8 irq_num;
     //uint32_t upper_bytes_addr, lower_bytes_addr;
+    int retval;
 
-    pr_alert("virtio EXAMPLE driver was loaded\n");
 
     /* get device IRQ number */
     //if(pci_read_config_byte(dev, PCI_INTERRUPT_LINE, &irq_num)) {
@@ -198,16 +237,38 @@ static int example_probe(struct virtio_device *dev)
     //iowrite32(lower_bytes_addr, dma_base);
     //iowrite32(upper_bytes_addr, (void*)((char*)dma_base + sizeof(uint32_t)));
 
-    /* creates a directory inside /sys/kernel for user, kobj = sysfs_dir */
-    example_kobj = kobject_create_and_add("example", kernel_kobj);
-    if (!example_kobj) {
-        pr_alert("failed to create a /sys/kernel directory for user\n");
+
+
+
+    ///* creates a directory inside /sys/kernel for user, kobj = sysfs_dir */
+    //example_kobj = kobject_create_and_add("example", kernel_kobj);
+    //if (!example_kobj) {
+    //    pr_alert("failed to create a /sys/kernel directory for user\n");
+    //}
+
+    ///* create sysfiles for user communication */
+    //if (sysfs_create_group(example_kobj, &example_attr_group)) {
+    //    pr_alert("failed to create sysfiles in /sys/kernel/dirname for user\n");
+    //}
+
+    retval = sysfs_create_group(kernel_kobj, &example_attr_group);
+    //retval = sysfs_create_group(hypervisor_kobj, &example_attr_group);
+    if (retval) {
+        pr_alert("failed to create group\n");
     }
 
-    /* create sysfiles for user communication */
-    if (sysfs_create_group(example_kobj, &example_attr_group)) {
-        pr_alert("failed to create sysfiles in /sys/kernel/dirname for user\n");
-    }
+
+
+    //FIXME: add error check
+	///* We expect a single virtqueue. */
+	//vq = virtio_find_single_vq(dev, example_irq_handler, "input");
+    //pr_alert("vq = %p\n", vq);
+	////if (IS_ERR(vi->vq)) {
+	////	err = PTR_ERR(vi->vq);
+	////	goto err_find;
+	////}
+
+    pr_alert("virtio EXAMPLE driver was loaded\n");
 
     return 0;
 }
@@ -215,14 +276,19 @@ static int example_probe(struct virtio_device *dev)
 //static void example_remove(struct pci_dev *dev)
 static void example_remove(struct virtio_device *dev)
 {
-    pr_alert("virtio EXAMPLE driver was removed\n");
     ////FIXME: call with the correct params
     ////devm_free_irq(dev);
     //dma_free_coherent(&dev->dev, DMA_BUF_SIZE, dma_buf_virtual_addr,
     //        dma_buf_physical_addr);
 
     /* remove the directory from sysfs */
-    kobject_del(example_kobj);
+    //kobject_del(example_kobj);
+    sysfs_remove_group(kernel_kobj, &example_attr_group);
+
+    /* disable interrupts for vqs */
+    dev->config->reset(dev);
+
+    pr_alert("virtio EXAMPLE driver was removed\n");
 }
 
 //static void virtrng_scan(struct virtio_device *vdev)
@@ -276,5 +342,4 @@ MODULE_DEVICE_TABLE(virtio, example_ids);
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Example virtio");
 MODULE_AUTHOR("Yoni Bettan");
-
 
