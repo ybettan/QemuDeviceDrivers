ff66b9466415441c37841f44a1b47281c0b96f4d
Yoni Bettan <ybettan@redhat.com>
2019-04-28 22:53:54 +0300

    Added GNU term license.
    Also removed a FIXME that was no longer needed.
    
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>

--- virtio/virtio_example_driver.c
+++ virtio/virtio_example_driver.c
@@ -1,3 +1,23 @@
+/*
+ * Virtio example implementation.
+ *
+ *  Copyright 2019 Yoni Bettan Red Hat Corporation
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #include <linux/virtio.h>
 #include <linux/module.h>
 #include <linux/device.h>
@@ -57,7 +77,6 @@ virtio_buf_store(struct device *dev, struct device_attribute *attr,
     request[0] = &sg_out;
     request[1] = &sg_in;
 
-    //FIXME: cange in_buf identifier ?
 	/* add the request to the queue, in_buf is sent as the buffer idetifier */
     virtqueue_add_sgs(vi->vq, request, 1, 1, &vi->in, GFP_KERNEL);
 
